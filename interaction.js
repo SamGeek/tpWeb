43b2bb1fe70d64a144f634ff87e4c63bb06921c8
Dagba Samuel Florent Mahou <samuel-florent-mahou.dagba@etudiant.univ-rennes1.fr>
2021-02-17 18:32:26 +0100

    ajout des models et gestion minimaliste de la vue

--- interaction.js
+++ interaction.js
@@ -2,16 +2,59 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  // Définir ici les attributs de la 'classe'
+  this.posInitialeX = 0;
+  this.posInitialeY = 0;
+  this.posFinaleX = 0;
+  this.posFinaleY = 0;
+  this.canvas = canvas;
+  console.log("Le canva original ",canvas)
+  console.log("mon canvas ",this.canvas)
+  this.isPressed = false;
+  // Developper les 3 fonctions gérant les événements
 
-	// Developper les 3 fonctions gérant les événements
+  // Associer les fonctions précédentes aux évènements du canvas.
+  this.canvas.addEventListener('mousedown', this.mouseDown.bind(this), false);
+  this.canvas.addEventListener('mousemove', this.mouseMove.bind(this), false);
+  this.canvas.addEventListener('mouseup', this.mouseUp.bind(this), false);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+};
+
+DnD.prototype.mouseDown = function (evt) {
+  var rect = getMousePosition(this.canvas, evt)
+  //console.log("mon canvas dans le down ",this.canvas)
+  this.posInitialeX = rect.x;
+  this.posInitialeY = rect.y;
+  this.isPressed = true;
+  //console.log("Les coordonnees sur le down sont"+rect.x+" "+rect.y)
+};
+
+
+DnD.prototype.mouseMove = function (evt) {
+  if(this.isPressed){
+    var rect = getMousePosition(this.canvas, evt)
+    this.posFinaleX = rect.x;
+    this.posFinaleY = rect.y;
+   // console.log("Les coordonnees sur le up sont"+rect.x+" "+rect.y)
+  }
+};
+
+
+DnD.prototype.mouseUp = function (evt) {
+  if(this.isPressed){
+    var rect = getMousePosition(this.canvas, evt)
+    this.posFinaleX = rect.x;
+    this.posFinaleY = rect.y;
+   // console.log("Les coordonnees sur le up sont"+rect.x+" "+rect.y)
+    this.isPressed = false;
+  }
+  
 };
 
 
 // Place le point de l'événement evt relativement à la position du canvas.
 function getMousePosition(canvas, evt) {
+ // console.log("le canvas recu en params ",canvas)
   var rect = canvas.getBoundingClientRect();
   return {
     x: evt.clientX - rect.left,
--- model.js
+++ model.js
@@ -1,3 +1,80 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+//Drawing
+
+function Drawing() {
+    // Définir ici les attributs de la 'classe'
+    this.forms = new Array();
+};
+
+//Shape // faire de lheritage avec les sous classes
+
+function Shape(initX, initY,finalX, finalY,thickness, color) {
+    // Définir ici les attributs de la 'classe'
+    this.color = color;
+    this.thickness = thickness;
+    this.initX = initX
+    this.initY = initY
+    this.finalX = finalX
+    this.finalY = finalY
+};
+
+//Rectangle
+function Rectangle(initX, initY, finalX, finalY ,thickness,color) {
+    // Définir ici les attributs de la 'classe'
+    Shape.call(this,initX, initY,finalX, finalY,thickness,color);
+
+    this.width = Math.abs(initX-finalX)
+    this.height = Math.abs(initY-finalY)
+};
+
+
+//Line
+function Line(initX, initY, finalX, finalY ,thickness,color) {
+    // Définir ici les attributs de la 'classe'
+    Shape.call(this,initX, initY,finalX, finalY,thickness, color);
+    
+};
+
+
+Shape.prototype.initPaint = function(ctx) {
+    //TODO Manager color
+    ctx.strokeStyle = this.color
+    ctx.strokeWidth = this.thickness
+  };
+
+
+Rectangle.prototype.paint = function(ctx) {
+    //TODO Manager color
+    //Done into the shape
+    //this.initPaint(ctx);
+    ctx.strokeStyle = this.color
+    ctx.strokeWidth = this.thickness
+    ctx.rect(this.initX, this.initY, this.finalX,   this.finalY);
+    ctx.stroke();
+  };
+  
+  Line.prototype.paint  = function(ctx) {
+    //TODO Manager color
+    //Done into the shape
+   // this.initPaint(ctx)
+    ctx.strokeStyle = this.color
+    ctx.strokeWidth = this.thickness
+    ctx.beginPath();
+    ctx.moveTo(this.initX, this.initY);
+    ctx.lineTo(this.finalX, this.finalY);
+    ctx.stroke();
+  };
+  
+  Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.forms.forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+    });
+  };
+  
